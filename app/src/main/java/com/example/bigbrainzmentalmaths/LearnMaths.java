ec3f83b62f5e54739792f59f7da5212ff4963aae
Kushal Galrani <kushal.galrani@gmail.com>
2020-05-08 10:11:51 +0530

    Created layout for Main Activity and Created new empty activity

---
+++ app/src/main/java/com/example/bigbrainzmentalmaths/LearnMaths.java
@@ -0,0 +1,14 @@
+package com.example.bigbrainzmentalmaths;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnMaths extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_maths);
+    }
+}
